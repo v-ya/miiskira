210c20896b24d121a44961ee2bb84f48d78ec754
vya <vya0x00@gmail.com>
2021-12-18 22:55:30 +0800

    2021-12-18 * define.h

--- define.h
+++ define.h
@@ -1,5 +1,7 @@
-#ifndef _miiskira_h_
-#define _miiskira_h_
+#ifndef _miiskira_define_h_
+#define _miiskira_define_h_
+
+// 常量定义
 
 #define miiskira$posky$posky  "posky"
 #define miiskira$posky$graph  "graph"
